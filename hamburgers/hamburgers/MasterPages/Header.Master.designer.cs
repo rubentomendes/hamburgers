f355cc81ab6e71de39fee3206b7b437cdf6774f8
ruben <120118009@esg.ipsantarem.pt>
2014-11-27 11:16:47 +0000

    bd base

--- hamburgers/hamburgers/MasterPages/Header.Master.designer.cs
+++ hamburgers/hamburgers/MasterPages/Header.Master.designer.cs
@@ -29,14 +29,5 @@ namespace hamburgers.MasterPages {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-        
-        /// <summary>
-        /// ContentPlaceHolder1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
     }
 }
