85502a305ec05cdaa6cffbe8dc660c4210cb5b9b
ruben <120118009@esg.ipsantarem.pt>
2014-12-01 18:59:07 +0000

    db mods

---
+++ hamburgers/hamburgers/admin/produtos.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace hamburgers.admin
+{
+    public partial class produtos : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            lblNome.Text = txtNome.Text;
+        }
+
+        protected void FormView1_PageIndexChanging(object sender, FormViewPageEventArgs e)
+        {
+
+        }
+    }
+}
